dfc7d27ccdea58234236cf0a2b7767e57cc4995e
ardian27 <ardiansyah2795@gmail.com>
2020-07-17 01:21:54 +0700

    test with lib php-ml

--- config/db.php
+++ config/db.php
@@ -4,9 +4,8 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=db_drugs',
     'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
+    'password' => 'root',
+    'charset' => 'utf8', 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,8 @@
 <?php
+// require_once __DIR__.'/vendor/autoload.php';
 
+use app\models\DataTransformasi;
+use Phpml\Clustering\KMeans;
 /* @var $this yii\web\View */
 
 $this->title = 'Drugs Mining';
@@ -12,6 +15,31 @@ $this->title = 'Drugs Mining';
 
     <div class="body-content">
 
+    <?php
+$kmean = new KMeans(2);
+
+$sample=[[1,21,22],[2,10,23],[1,11,29],[2,4,99],[1,21,21],[2,10,22],[1,11,32],[2,4,12],[1,21,22],[2,10,23],[1,11,29],[2,4,99],[1,21,21],[2,10,22],[1,11,32],[2,4,12]];
+$dataDB = DataTransformasi::find()->select('umur,jenis_kelamin,ras,jenis_obat_1,jenis_obat_2,jenis_obat_3,jenis_obat_4,jenis_obat_5,jenis_obat_6,jenis_obat_7')->asArray()->all();
+$result = array_map('array_values', $dataDB);
+$samples = [[1, 1], [8, 7], [1, 2], [7, 8], [2, 1], [8, 9]];
+
+$kmeans = new KMeans(5);
+// $kmeans = new KMeans(5,5);
+$r=$kmeans->cluster($result);
+
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r[0]), true) . "</pre> "); 
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r[1]), true) . "</pre> "); 
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r[2]), true) . "</pre> "); 
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r[3]), true) . "</pre> "); 
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r[4]), true) . "</pre> "); 
+
+// print("<p>Data Dummy <pre> " . print_r(sizeOf($r), true) . "</pre> "); 
+
+// print("<p>Data Dummy <pre> " . print_r($result, true) . "</pre> "); 
+print("<p>Data Dummy <pre> " . print_r($r, true) . "</pre> "); 
+
+
+?>
         <div class="jumbotron">
             <div class="col-lg-12 center">
             <img src="img/uin.jpg"height="100" width="100">
@@ -21,6 +49,8 @@ $this->title = 'Drugs Mining';
                 <h4>UIN Suska Riau</h4>                
             </div>
         </div>
+
+
 
     </div>
 </div>
